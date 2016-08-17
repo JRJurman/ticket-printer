adc47ef0408bd0837db53b56767a17e2541e5d04
jjurman <jjurman@constantcontact.com>
2016-08-17 17:38:37 -0400

    led indicators for printing, ping, and startup

--- webserver.js
+++ webserver.js
@@ -45,6 +45,51 @@ catch(e) {
   printer = require('./src/console-printer.js');
 }
 
+var blinkLeds;
+// function to blink the tessel leds on and off
+function blink() {
+  if (tessel) {
+    tessel.led[1].on();
+    tessel.led[2].on();
+    tessel.led[3].on();
+    blinkLeds = setInterval(function() {
+      tessel.led[1].toggle();
+      tessel.led[2].toggle();
+      tessel.led[3].toggle();
+    }, 100);
+    setTimeout(function() {
+      clearInterval(blinkLeds);
+      tessel.led[1].off();
+      tessel.led[2].off();
+      tessel.led[3].off();
+    }, 2500)
+  }
+}
+
+var patternLeds;
+// function to animate the tessel leds in a pattern
+function pattern() {
+  if (tessel) {
+    var switches = ['on', 'off', 'off'];
+    tessel.led[1][switches[0]]();
+    tessel.led[2][switches[1]]();
+    tessel.led[3][switches[2]]();
+    patternLeds = setInterval(function() {
+      // rotate the switches 
+      switches.unshift(switches.pop())
+      tessel.led[1][switches[0]]();
+      tessel.led[2][switches[1]]();
+      tessel.led[3][switches[2]]();
+    }, 100);
+    setTimeout(function() {
+      clearInterval(patternLeds)
+      tessel.led[1].off();
+      tessel.led[2].off();
+      tessel.led[3].off();
+    }, 4500);
+  }
+}
+
 // allow cross origin requests for our print endpoint
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -54,11 +99,17 @@ app.use(function(req, res, next) {
 
 // make sure we have the right server
 app.get('/ping', function(req, res) {
+  // send nothing, we exist, isn't that enough?!?!
   res.sendStatus(204);
+  // blink on the tessel, because we asked if we were here!
+  blink();
 });
 
 // print endpoint that triggers a print
 app.post('/print', function (req, res, next) {
+
+  // do a fancy pattern on the tessel, because we were told to print!
+  pattern();
 
   // validate the ticket exists, and has the things we want
   var ticket = req.body;
@@ -98,6 +149,8 @@ app.post('/print', function (req, res, next) {
 // Start the server
 var port = 3000;
 app.listen(port, function () {
+  // blink on the tessel that we're running!
+  blink();
 
   // log what our ip is on the console
   console.log('Point of Tickets listening on '+ip+':'+port);
