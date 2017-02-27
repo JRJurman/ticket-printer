6d4da888f5f1e71d3e9f7f70e3560c3e6bb6e916
jjurman <jjurman@constantcontact.com>
2017-02-27 16:48:44 -0500

    test command, webserver improvements, log command
    - webserver blocks multiple fast requests
    - exposes log endpoint to print interactions
    - throws 400 for malformed data

--- webserver.js
+++ webserver.js
@@ -2,52 +2,63 @@
 // a webserver that exposes endpoints to trigger the printer
 
 // network dependency
-var os = require('os');
+const os = require('os');
 
 // figure out what network we're on, so we can display it to the user
-var networks = os.networkInterfaces();
+const networks = os.networkInterfaces();
 // flatten all the possible address into one array
-var addresses = Object.keys(networks).reduce(function(addrs, interface) {
+const addresses = Object.keys(networks).reduce(function(addrs, interface) {
   // add all the address we found on this interface to the list of addresses
   return addrs.concat(networks[interface].map(function(i) {return i.address;}));
 }, []);
 
+// if we pass 'test' as a parameter, then don't connect to the tessel
+const testing = process.argv[2] == 'test';
+
 // regex to find the one true ip address
-var ipLike = /\d+\.\d+.\d+.\d+/
-var ip = addresses.filter(function(address) {
+const ipLike = /\d+\.\d+.\d+.\d+/
+const ip = addresses.filter(function(address) {
   return (ipLike.test(address) && address !== "127.0.0.1");
 })[0];
 
 // webserver dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // setup webserver
-var app = express();
+const app = express();
 app.use(bodyParser.json());
 
+// log, to keep track of malicious users
+const log = [];
+
+var tessel, thermalprinter;
 // printing dependencies
-var printer;
+const printer = (() => {
+  // try to pull in tessel packages
+  try {
+    if (testing) {
+      tessel = null;
+      throw '';
+    }
+    tessel = require('tessel');
+    thermalprinter = require('tessel-thermalprinter');
 
-// try to pull in tessel packages
-try {
-  var tessel = require('tessel');
-  var thermalprinter = require('tessel-thermalprinter');
+    // setup printer
+    return thermalprinter.use(tessel.port['A']);
+  }
+  catch(e) {
+    console.warn("couldn't find tessel packages...");
+    console.warn("if you're testing, this is perfectly normal.");
 
-  // setup printer
-  printer = thermalprinter.use(tessel.port['A']);
-}
-catch(e) {
-  console.warn("couldn't find tessel packages...");
-  console.warn("if you're testing, this is perfectly normal.");
+    // if we're testing, or don't have a tessel, fall back on the console
+    return require('./src/console-printer.js');
+  }
+})();
 
-  // if we're testing, or don't have a tessel, fall back on the console
-  printer = require('./src/console-printer.js');
-}
-
-var blinkLeds;
 // function to blink the tessel leds on and off
 function blink() {
+  var blinkLeds;
   if (tessel) {
     tessel.led[1].on();
     tessel.led[2].on();
@@ -66,16 +77,16 @@ function blink() {
   }
 }
 
-var patternLeds;
 // function to animate the tessel leds in a pattern
 function pattern() {
+  var patternLeds;
   if (tessel) {
     var switches = ['on', 'off', 'off'];
     tessel.led[1][switches[0]]();
     tessel.led[2][switches[1]]();
     tessel.led[3][switches[2]]();
     patternLeds = setInterval(function() {
-      // rotate the switches 
+      // rotate the switches
       switches.unshift(switches.pop())
       tessel.led[1][switches[0]]();
       tessel.led[2][switches[1]]();
@@ -108,37 +119,72 @@ app.get('/ping', function(req, res) {
 // print endpoint that triggers a print
 app.post('/print', function (req, res, next) {
 
-  // do a fancy pattern on the tessel, because we were told to print!
-  pattern();
+  if (log.length > 0) {
+    if (  (req.ip == log[log.length - 1].ip) &&
+          (((new Date) - (log[log.length - 1].timestamp)) < 5000)
+    ) {
+      res.sendStatus(429);
+      return;
+    }
+  }
+  log.push({type: 'ticket', ip: req.ip, timestamp: new Date});
 
   // validate the ticket exists, and has the things we want
-  var ticket = req.body;
+  const ticket = Object.assign({}, req.body);
+  if (!ticket.project || !ticket.number || !ticket.title || !ticket.author ||
+      !ticket.created || !ticket.body) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const limits = {
+    project: 30,
+    number: 30,
+    title: 30,
+    author: 30,
+    created: 30,
+    body: 2000
+  };
+
+  const truncatedTicket = Object.keys(limits).reduce((tinyTicket, prop) => {
+    if (tinyTicket[prop].length > limits[prop]) {
+      return Object.assign(
+        {}, tinyTicket,
+        {[prop]: tinyTicket[prop].slice(0, limits[prop]-3).concat('...')}
+      );
+    } else {
+      return tinyTicket;
+    }
+  }, ticket);
+
+  // do a fancy pattern on the tessel, because we were told to print!
+  pattern();
 
   // print the JSON
   printer
     .center()
     .horizontalLine(32)
     .inverse(true)
-    .printLine(" " + ticket.project + " ")
+    .printLine(" " + truncatedTicket.project + " ")
     .inverse(false)
     .lineFeed(1)
     .bold(true)
-    .printLine(ticket.number)
-    .printLine(ticket.title)
+    .printLine(truncatedTicket.number)
+    .printLine(truncatedTicket.title)
     .bold(false)
     .lineFeed(1)
     .left(true)
     .printLine("Author:")
     .right(true)
-    .printLine(ticket.author)
+    .printLine(truncatedTicket.author)
     .left(true)
     .printLine("Created on:")
     .right(true)
-    .printLine(ticket.created)
+    .printLine(truncatedTicket.created)
     .left(true)
     .lineFeed(1)
     .horizontalLine(32)
-    .printLine(ticket.body)
+    .printLine(truncatedTicket.body)
     .lineFeed(7)
     .print(function() {
       res.sendStatus(204);
@@ -146,14 +192,46 @@ app.post('/print', function (req, res, next) {
 
 });
 
+// print endpoint that prints the log
+app.get('/paranoia', function (req, res, next) {
+
+  if (log.length > 0) {
+    if (  (req.ip == log[log.length - 1].ip) &&
+          (((new Date) - (log[log.length - 1].timestamp)) < 5000)
+    ) {
+      res.sendStatus(429);
+      return;
+    }
+  }
+  log.push({type: 'log', ip: req.ip, timestamp: new Date});
+
+  // do a fancy pattern on the tessel, because we were told to print!
+  pattern();
+
+  // print the log
+  const queuedPrinter = log.reduce((printerObject, event) => {
+    return printerObject
+              .horizontalLine(16)
+              .left()
+              .printLine(event.ip)
+              .right()
+              .printLine(event.timestamp);
+  }, printer);
+  queuedPrinter
+    .print(function() {
+      res.sendStatus(204);
+    });
+
+});
+
 // Start the server
-var port = 3000;
+var port = 1335;
 app.listen(port, function () {
   // blink on the tessel that we're running!
   blink();
 
   // log what our ip is on the console
-  console.log('Point of Tickets listening on '+ip+':'+port);
+  console.log('Ticket-Printer listening on '+ip+':'+port);
 
   // sometimes, we might not have a console, so this will work too
   printer.on('ready', function(){
